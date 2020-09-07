084e96b3ad4079444d9d969dd63734b99e66ea4c
tonitingaurav <tonitingaurav@gmail.com>
2020-09-07 17:00:55 +0530

    Sample Rest Application

---
+++ src/main/docker/run.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "********************************************************"
+echo "Starting simple-service "
+echo "********************************************************"
+java -jar /usr/local/dist/demo/@project.build.finalName@.jar
